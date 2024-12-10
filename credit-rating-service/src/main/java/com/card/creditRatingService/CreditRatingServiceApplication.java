d53e15e0296428ad9a3b4d4a67baec070232df5f
prasannapadole <padoleprasanna@gmail.com>
2024-12-10 15:01:46 +0530

    initial code setup

---
+++ credit-rating-service/src/main/java/com/card/creditRatingService/CreditRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.card.creditRatingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditRatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditRatingServiceApplication.class, args);
+	}
+
+}
---
+++ credit-rating-service/src/test/java/com/card/creditRatingService/CreditRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.card.creditRatingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
